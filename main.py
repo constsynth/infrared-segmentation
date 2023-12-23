31536d07b7cebba58eb282db9b2ebc3066955eed
constsynth <constsynth@gmail.com>
2023-12-24 02:49:07 +0700

    try exception for "no detections found" case

--- main.py
+++ main.py
@@ -33,29 +33,31 @@ def tracking(rtsp_address: str = None, path_to_model: str = None):
 
         if success:
             results = model.track(frame, persist=True, conf=0.3, tracker='bytetrack.yaml')
+            try:
+                boxes = results[0].boxes.xywh.cpu()
+                track_ids = results[0].boxes.id.int().cpu().tolist()
 
-            boxes = results[0].boxes.xywh.cpu()
-            track_ids = results[0].boxes.id.int().cpu().tolist()
+                annotated_frame = results[0].plot()
 
-            annotated_frame = results[0].plot()
+                for box, track_id in zip(boxes, track_ids):
+                    x, y, w, h = box
+                    track = track_history[track_id]
+                    track.append((int(x), int(y)))
+                    if len(track) >= 2:
+                        speed = estimate_speed(track[-1], track[-2], ppm_rate=16)
+                        cv2.putText(annotated_frame, f'{speed} km/h', track[-1], font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                    if len(track) > 50:
+                        track.pop(0)
 
-            for box, track_id in zip(boxes, track_ids):
-                x, y, w, h = box
-                track = track_history[track_id]
-                track.append((int(x), int(y)))
-                if len(track) >= 2:
-                    speed = estimate_speed(track[-1], track[-2], ppm_rate=16)
-                    cv2.putText(annotated_frame, f'{speed} km/h', track[-1], font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                if len(track) > 50:
-                    track.pop(0)
+                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                    cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
 
-                points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-                cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+                cv2.imshow("infrared image tracking", annotated_frame)
 
-            cv2.imshow("infrared image tracking", annotated_frame)
-
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+            except AttributeError:
+                continue
         else:
             break
     cap.release()
@@ -63,4 +65,4 @@ def tracking(rtsp_address: str = None, path_to_model: str = None):
 
 
 if __name__ == "__main__":
-    tracking(rtsp_address='30 Minutes of Cars Driving By in 2009.mp4', path_to_model='./yolov8s-seg.pt')
+    tracking(rtsp_address='people_thermal.mp4', path_to_model='./trained_detection/best.pt')
